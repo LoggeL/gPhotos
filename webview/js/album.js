f967bb87ce3571af10330691eed65eb4e36f6af3
LoggeL <hyper.xjo@gmx.de>
2021-09-27 16:52:00 +0200

    decrease margin

--- webview/js/album.js
+++ webview/js/album.js
@@ -5,6 +5,7 @@ fetch('api/album' + albumID).then(response => response.json().then(pictures => {
     const imageData = pictures.map(picture => ({ filename: picture.url, aspectRatio: picture.width / picture.height }))
 
     new Pig(imageData, {
+        spaceBetweenImages: 5,
         urlForSize: function (filename, size) {
             if (size == "20") return '/assets/thumb.svg'
             return filename + "=w" + size * window.devicePixelRatio;
