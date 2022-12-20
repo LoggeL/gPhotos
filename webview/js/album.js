8405e1eacee29d75215b0fa21d937844bfbd1838
Logge <hyper.xjo@gmx.de>
2022-12-20 16:23:50 +0100

    fix float in width parameter

--- webview/js/album.js
+++ webview/js/album.js
@@ -1,18 +1,26 @@
 const albumID = window.location.pathname
 
-fetch('api/album' + albumID).then(response => response.json().then(pictures => {
+fetch('api/album' + albumID)
+  .then((response) =>
+    response.json().then((pictures) => {
+      const imageData = pictures.map((picture) => ({
+        filename: picture.url,
+        aspectRatio: picture.width / picture.height,
+      }))
 
-    const imageData = pictures.map(picture => ({ filename: picture.url, aspectRatio: picture.width / picture.height }))
-
-    new Pig(imageData, {
+      new Pig(imageData, {
         spaceBetweenImages: 5,
         urlForSize: function (filename, size) {
-            if (size == "20") return '/assets/thumb.svg'
-            return filename + "=w" + size * window.devicePixelRatio;
+          if (size == '20') return '/assets/thumb.svg'
+          return filename + '=w' + parseInt(size * window.devicePixelRatio)
         },
         onClickHandler: function (filename) {
-            const width = pictures.find(picture => picture.url === filename).width
-            window.open(filename + '=w' + width, '_blank')
+          const width = pictures.find(
+            (picture) => picture.url === filename
+          ).width
+          window.open(filename + '=w' + width, '_blank')
         },
-    }).enable();
-})).catch(console.error)
+      }).enable()
+    })
+  )
+  .catch(console.error)
